67c1f700d6cad266275ec8388bf07b240b151f06
Davin DJ <davindx88@gmail.com>
2022-11-01 15:03:51 +0700

    nvim-tree - Add Line Number

--- lua/davindj/qol/nvim-tree.lua
+++ lua/davindj/qol/nvim-tree.lua
@@ -3,6 +3,8 @@ require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
         adaptive_size = true,
+        number = true,
+        relativenumber = true,
         mappings = {
             list = {
                 { key = "u", action = "dir_up" },
